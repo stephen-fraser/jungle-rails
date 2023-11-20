fcd85b8c9d7e9ab52ff1b778713ab3cae0a11c44
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-20 15:14:38 -0800

    add initial user_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   describe 'Validations' do
+
+#     it 'is not valid when a password and passoword_confirmation do not match'
+
+#   end
+# end
+
